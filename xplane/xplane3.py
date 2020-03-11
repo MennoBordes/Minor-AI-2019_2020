cebeaee6dea853e9d221111503e768017788ed11
Skillerde6de <mennobordes@hetnet.nl>
2020-03-11 19:21:50 +0100

    Added basic example for xpc

--- xplane/xplane3.py
+++
@@ -1,7 +0,0 @@
-import socket
-MCAST_GRP = "145.137.9.149"
-MCAST_PORT = 49000
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-sock.sendto(b"test", (MCAST_GRP, MCAST_PORT))
