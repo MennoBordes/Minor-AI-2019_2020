ffc687efde293ba6d2db32391007e26624bec105
Menno Bordes <mennobordes@hetnet.nl>
2020-05-12 19:40:20 +0200

    Init gym-xplane environment

---
+++ gym-xplane/gym_xplane/envs/xplane_env.py
@@ -0,0 +1,19 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+
+class XplaneENV(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+        value = "not yet implemented"
+
+    def step(self, action):
+        return "not yet implemented"
+
+    def reset(self):
+        return "not yet implemented"
+
+    def render(self, mode='human', close=False):
+        return "not yet implemented"
---
+++ gym-xplane/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='gym-xplane',
+      version='0.0.1',
+      install_requires=['gym']  # And any other dependencies required
+      )
