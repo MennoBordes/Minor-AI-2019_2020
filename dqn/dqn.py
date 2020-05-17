6333defef64ee2bb992c6617a100510edadb47db
FelixdeJonge <felixdejonge@gmail.com>
2020-05-17 11:25:29 +0200

    Update dqn.py

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -1,19 +1,29 @@
-import tensorflow as tf
-from tf_agents.networks import q_network
-from tf_agents.agents.dqn import dqn_agent
-from environment.xplane_env import XPlaneEnv
+# import tensorflow as tf
+import numpy as np
+import random
+import os
+import gym
+import gym_xplane
 
-q_net = q_network.QNetwork(
-  XPlaneEnv.observation_spec(),
-  XPlaneEnv.action_spec(),
-  fc_layer_params=(100,))
+'''
+CWD = os.getcwd()
+# print(CWD)
 
-agent = dqn_agent.DqnAgent(
-  XPlaneEnv.time_step_spec(),
-  XPlaneEnv.action_spec(),
-  q_network=q_net,
-  optimizer=optimizer,
-  td_errors_loss_fn=common.element_wise_squared_loss,
-  train_step_counter=tf.Variable(0))
 
-agent.initialize()
+class DQModel(tf.keras.Model):
+    def __init__(self, hidden_size: int, num_actions: int):
+        super(DQModel, self).__init__()
+        self.ml1 = tf.keras.layers.Input
+
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(32, input_shape=(16,)))
+model.add(tf.keras.layers.Dense(32))
+
+print(model)
+'''
+
+env = gym.make('xplane-gym-v0')
+print(env)
+env.reset()
+env.close()
