8ae0623755dafd787eb008df42eabaf3caa5cdca
Menno Bordes <mennobordes@hetnet.nl>
2020-06-02 12:13:55 +0200

    Remove legacy

--- gym-xplane/gym_xplane/envs/xplane_env.py
+++ gym-xplane/gym_xplane/envs/xplane_env.py
@@ -109,48 +109,6 @@ class XplaneENV(gym.Env):
         XplaneENV.CLIENT.sendDREF("sim/time/zulu_time/sec", 32400.0)
         # XplaneENV.CLIENT.sendDREF("sim/cockpit/switches/gear_handle_status", 1)
 
-        # Legacy for xpc.py
-        if False:
-            with xpc.XPlaneConnect() as client:
-                client.resetPlane()
-
-                # Reset time to 10:00 (32400.0)
-                client.sendDREF("sim/time/zulu_time_sec", 32400.0)
-
-                return
-                # client.sendDREF("sim/operation/fix_all_systems", 1)
-                client.sendDREF("sim/cockpit/switches/gear_handle_status", 1)
-                client.sendDREF("sim/cockpit2/controls/gear_handle_down", 1)
-
-                # Reset position of the player aircraft
-                #       Lat               Lon                Alt            Pitch Roll Yaw Gear
-                posi = [52.3286247253418, 4.708916664123535, -0.315114825963974, 0, 0, 0, 1]
-                client.sendPOSI(posi)
-
-                ctrl = [0.0, 0.0, 0.0, 0.0, 1, 0]
-                client.sendCTRL(ctrl)
-
-                '''
-                (3) Reset velocity
-                (17) Reset heading
-                (62) Reset fuel 
-                '''
-                data = [ \
-                    [3, 0, 0, 0, 0, -998, -998, -998, -998], \
-                    [17, 0, 0, 3.1591200828552246, 0, -998, -998, -998, -998], \
-                    [62, 37515, 14938, 42118, 42132, 14938, 4404.9, 4404.9, 11107], \
-                    ]
-                client.sendDATA(data)
-                # Reset time to 10:00 (32400.0)
-                client.sendDREF("sim/time/zulu_time_sec", 32400.0)
-                # Re-apply parking brake
-                client.sendDREF("sim/cockpit2/controls/parking_brake_ratio", 1)
-                # Re-apply landing gear switch
-                client.sendDREF("sim/cockpit2/controls/gear_handle_down", 1)
-
-                # print("Set camera")
-                client.sendVIEW(xpc.ViewType.Chase)
-                sleep(1)
 
     def _get_info(self):
         """Returns a dictionary containing debug info."""
@@ -187,4 +145,3 @@ class XplaneENV(gym.Env):
 
     def remove_waypoints(self):
         XplaneENV.CLIENT.sendWYPT(op=3, points=[])
-        pass
