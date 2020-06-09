d812d50ca5fe42056303d1385ed5703b633e05e6
Hayder1999 <hayderali1999@gmail.com>
2020-06-09 14:51:47 +0200

    Basic DeepQ + 0.50% reset done

--- DeepQ/main.py
+++ DeepQ/main.py
@@ -1,34 +1,3 @@
 import gym
-import numpy as np 
-# from dqn import Agent
 
 gym_env = gym.make('custom_gym:Xplane-v0')
-# gym_env.reset()
-# gym_env.test_window()
-gym_env.test_window2()
-# gym_env.test_keystroke()
-
-# lr = 0.001c
-# gam = 0.99
-# n_games = 1000
-# agent = Agent(learning_rate=lr, gamma=gam, epsilon=1.0, p
-#     input_dims=gym_env.observation_space.shape, n_actions=gym_env.action_space.n, batch_size=70, file_name='dq_model_1.h5')
-# scores = []
-# eps_hist = []
-
-# for i in range(n_games):
-#     crashed = False
-#     score = 0 
-#     observation = gym_env.reset()
-#     while not crashed:
-#         action = agent.choose_action(observation)
-#         new_observation, reward, done, info = gym_env.step(action)
-#         score = score + reward
-#         agent.store_transition(observation, action, reward, new_observation, crashed)
-#         observation = new_observation
-#         agent.learn()
-#     eps_hist.append(agent.epsilon)
-#     scores.append(score)
-    
-
-
--- customGym/custom_gym/envs/xplEnv.py
+++ customGym/custom_gym/envs/xplEnv.py
@@ -1,13 +1,6 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
-from custom_gym.envs.myxpc.XPlaneFunctions import send_waypoints
-from custom_gym.envs.myxpc import xpc2 as xpc
-from custom_gym.envs.myxpc import keypress
-from custom_gym.envs.myxpc.keypress import ResetXPlane
-import pygetwindow
-import pydirectinput
-import time
 
 
 class XPL(gym.Env):
@@ -19,60 +12,11 @@ class XPL(gym.Env):
     def step(self, action):
         print("step")
 
-    def reward(self):
-        print("reward")
     def reset(self):
         print("reset")
-        # Set simulation speed for faster training
-        print("Setting up simulation")
-        with xpc.XPlaneConnect() as client:
-            # Verify connection
-            try:
-                # If X-Plane does not respond to the request, a timeout error
-                # will be raised.
-                client.getDREF("sim/test/test_float")
-            except:
-                print("Error establishing connection to X-Plane.")
-                print("Exiting...")
-                return
-            simulation_dref = "sim/time/sim_speed"
-            client.sendDREF(simulation_dref, 1000)
-            res = client.getDREF(simulation_dref)
-            print(res)
-        # Reset airplane
-        xplane_window = pygetwindow.getWindowsWithTitle("X-System")
-        print(xplane_window)
 
     def render(self, mode="human"):
         print("render")
 
     def quit(self):
         print("quit")
-
-
-    def test_window(self):
-        
-        xplane_window = pygetwindow.getWindowsWithTitle("X-System")[0]
-        xplane_window.activate()
-        time.sleep(4)
-        keypress.PressKey(0x50)
-        time.sleep(1)
-        keypress.ReleaseKey(0x50)
-    
-    def test_keystroke(self):
-        keypress.PressKey(0x50)
-
-
-    def test_window2(self):
-        xplane_window = pygetwindow.getWindowsWithTitle("X-System")[0]
-        xplane_window.activate()
-        time.sleep(6)
-        pydirectinput.keyDown('p')
-        time.sleep(1)
-        pydirectinput.keyUp('p')
-
-    def test_window3(self):
-        xplane_window = pygetwindow.getWindowsWithTitle("X-System")[0]
-        xplane_window.activate()
-        time.sleep(2)
-        ResetXPlane()
