df04624d945eef244a5a399a500ed4b74eaf79fa
Menno Bordes <mennobordes@hetnet.nl>
2020-07-04 21:20:35 +0200

    Update current_training_model.py

--- DQN/current_training_model.py
+++ DQN/current_training_model.py
@@ -1,7 +1,7 @@
 from gym_xplane.envs.xplane_env import AI_type
 
-training_Landing = AI_type.Landing
-training_Cruise = AI_type.Cruise
 training_TakeOff = AI_type.TakeOff
+training_Cruise = AI_type.Cruise
+training_Landing = AI_type.Landing
 
 current_training = training_Cruise
